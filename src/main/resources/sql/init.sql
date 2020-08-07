7a587930f20d70dec165dfb77a93784defb049e3
DiangD <1304138666@qq.com>
2020-08-07 13:10:14 +0800

    update init.sql

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -1,147 +1,201 @@
-# 数据库：MySQL
-# 版本：8.0
+/*
+ Navicat MySQL Data Transfer
 
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80015
+ Source Host           : localhost:3306
+ Source Schema         : epidemic_ncov
 
-drop table if exists `province_info`;
-# 省份信息表
-create table `province_info`
+ Target Server Type    : MySQL
+ Target Server Version : 80015
+ File Encoding         : 65001
+
+ Date: 07/08/2020 12:27:43
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for city_info
+-- ----------------------------
+DROP TABLE IF EXISTS `city_info`;
+CREATE TABLE `city_info`
 (
-    `locationId`            int(11)      not null,
-    `provinceName`          varchar(255) null default null,
-    `provinceShortName`     varchar(255) null default null,
-    `currentConfirmedCount` int(11)      null default null,
-    `confirmedCount`        int(11)      null default null,
-    `suspectedCount`        int(11)      null default null,
-    `curedCount`            int(11)      null default null,
-    `deadCount`             int(11)      null default null,
-    `comment`               varchar(255) null default null,
-    `statisticsData`        varchar(255) null default null,
-    `modifyTime`            timestamp    null default now(),
-    primary key (`locationId`) using btree
-) engine = InnoDB
-  default charset = utf8;
+    `locationId`            int(11)                                                 NOT NULL,
+    `cityName`              varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    `confirmedCount`        int(11)                                                 NULL DEFAULT NULL,
+    `suspectedCount`        int(11)                                                 NULL DEFAULT NULL,
+    `curedCount`            int(11)                                                 NULL DEFAULT NULL,
+    `deadCount`             int(11)                                                 NULL DEFAULT NULL,
+    `modifyTime`            timestamp(0)                                            NULL DEFAULT CURRENT_TIMESTAMP,
+    `provinceId`            int(11)                                                 NOT NULL,
+    `currentConfirmedCount` int(11)                                                 NULL DEFAULT NULL,
+    PRIMARY KEY (`locationId`) USING BTREE,
+    INDEX `index_city_info` (`provinceId`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
 
-drop table if exists `city_info`;
-# 城市信息表
-create table `city_info`
+-- ----------------------------
+-- Table structure for country_info
+-- ----------------------------
+DROP TABLE IF EXISTS `country_info`;
+CREATE TABLE `country_info`
 (
-    `locationId`     int(11)      not null,
-    `cityName`       varchar(255) null default null,
-    `confirmedCount` int(11)      null default null,
-    `suspectedCount` int(11)      null default null,
-    `curedCount`     int(11)      null default null,
-    `deadCount`      int(11)      null default null,
-    `modifyTime`     timestamp    null default now(),
-    `provinceId`     int(11)      not null,
-    primary key (`locationId`) using btree
+    `locationId`            int(11)                                                 NOT NULL,
+    `provinceName`          varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    `provinceShortName`     varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    `currentConfirmedCount` int(11)                                                 NULL DEFAULT NULL,
+    `confirmedCount`        int(11)                                                 NULL DEFAULT NULL,
+    `suspectedCount`        int(11)                                                 NULL DEFAULT NULL,
+    `curedCount`            int(11)                                                 NULL DEFAULT NULL,
+    `deadCount`             int(11)                                                 NULL DEFAULT NULL,
+    `modifyTime`            bigint(20)                                              NULL DEFAULT NULL,
+    `createTime`            bigint(20)                                              NULL DEFAULT NULL,
+    `countryShortCode`      varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    `countryType`           int(2)                                                  NULL DEFAULT NULL,
+    `continents`            varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    PRIMARY KEY (`locationId`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
 
-) engine = InnoDB
-  default charset = utf8;
-
-drop table if exists `statistic_data`;
-create table `statistic_data`
+-- ----------------------------
+-- Table structure for global_statistics
+-- ----------------------------
+DROP TABLE IF EXISTS `global_statistics`;
+CREATE TABLE `global_statistics`
 (
-    `id`                    int(11) not null auto_increment,
-    `confirmedCount`        int(11) null default null,
-    `confirmedIncr`         int(11) null default null,
-    `curedCount`            int(11) null default null,
-    `curedIncr`             int(11) null default null,
-    `currentConfirmedCount` int(11) null default null,
-    `currentConfirmedIncr`  int(11) null default null,
-    `dateId`                int(11) null default null,
-    `deadCount`             int(11) null default null,
-    `deadIncr`              int(11) null default null,
-    `provinceId`            int(11) null default null,
-    primary key (`id`) using btree
-) engine = InnoDB
-  default charset = utf8;
+    `id`                    int(11) NOT NULL,
+    `confirmedCount`        int(11) NULL DEFAULT NULL,
+    `curedCount`            int(11) NULL DEFAULT NULL,
+    `deadCount`             int(11) NULL DEFAULT NULL,
+    `confirmedIncr`         int(11) NULL DEFAULT NULL,
+    `curedIncr`             int(11) NULL DEFAULT NULL,
+    `deadIncr`              int(11) NULL DEFAULT NULL,
+    `currentConfirmedCount` int(11) NULL DEFAULT NULL,
+    `currentConfirmedIncr`  int(11) NULL DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
 
-
-# 统计信息
-drop table if exists `statistic`;
-create table `statistic`
+-- ----------------------------
+-- Table structure for province_info
+-- ----------------------------
+DROP TABLE IF EXISTS `province_info`;
+CREATE TABLE `province_info`
 (
-    id                    int(11)    not null,
-    createTime            bigint(20) null default null,
-    modifyTime            bigint(20) null default null,
-    confirmedCount        int(11)    null default null,
-    suspectedCount        int(11)    null default null,
-    curedCount            int(11)    null default null,
-    deadCount             int(11)    null default null,
-    seriousCount          int(11)    null default null,
-    suspectedIncr         int(11)    null default null,
-    confirmedIncr         int(11)    null default null,
-    curedIncr             int(11)    null default null,
-    deadIncr              int(11)    null default null,
-    seriousIncr           int(11)    null default null,
-    currentConfirmedCount int(11)    null default null,
-    currentConfirmedIncr  int(11)    null default null,
-    primary key (`id`) using btree
+    `locationId`            int(11)                                                 NOT NULL,
+    `provinceShortName`     varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    `currentConfirmedCount` int(11)                                                 NULL DEFAULT NULL,
+    `confirmedCount`        int(11)                                                 NULL DEFAULT NULL,
+    `suspectedCount`        int(11)                                                 NULL DEFAULT NULL,
+    `curedCount`            int(11)                                                 NULL DEFAULT NULL,
+    `deadCount`             int(11)                                                 NULL DEFAULT NULL,
+    `comment`               varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    `statisticsData`        varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    `modifyTime`            timestamp(0)                                            NULL DEFAULT CURRENT_TIMESTAMP,
+    `provinceName`          varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    PRIMARY KEY (`locationId`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
 
-) engine = InnoDB
-  default charset = utf8;
-
-drop table if exists `global_statistics`;
-create table `global_statistics`
+-- ----------------------------
+-- Table structure for statistic
+-- ----------------------------
+DROP TABLE IF EXISTS `statistic`;
+CREATE TABLE `statistic`
 (
-    id                    int(11) not null,
-    confirmedCount        int(11) null default null,
-    curedCount            int(11) null default null,
-    deadCount             int(11) null default null,
-    confirmedIncr         int(11) null default null,
-    curedIncr             int(11) null default null,
-    deadIncr              int(11) null default null,
-    currentConfirmedCount int(11) null default null,
-    currentConfirmedIncr  int(11) null default null,
-    primary key (`id`) using btree
-) engine = InnoDB
-  default charset = utf8;
+    `id`                    int(11)    NOT NULL,
+    `createTime`            bigint(20) NULL DEFAULT NULL,
+    `modifyTime`            bigint(20) NULL DEFAULT NULL,
+    `confirmedCount`        int(11)    NULL DEFAULT NULL,
+    `suspectedCount`        int(11)    NULL DEFAULT NULL,
+    `curedCount`            int(11)    NULL DEFAULT NULL,
+    `deadCount`             int(11)    NULL DEFAULT NULL,
+    `seriousCount`          int(11)    NULL DEFAULT NULL,
+    `suspectedIncr`         int(11)    NULL DEFAULT NULL,
+    `confirmedIncr`         int(11)    NULL DEFAULT NULL,
+    `curedIncr`             int(11)    NULL DEFAULT NULL,
+    `deadIncr`              int(11)    NULL DEFAULT NULL,
+    `seriousIncr`           int(11)    NULL DEFAULT NULL,
+    `currentConfirmedCount` int(11)    NULL DEFAULT NULL,
+    `currentConfirmedIncr`  int(11)    NULL DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
 
-# 时间线
-drop table if exists `timeline`;
-create table timeline
+-- ----------------------------
+-- Table structure for statistic_data
+-- ----------------------------
+DROP TABLE IF EXISTS `statistic_data`;
+CREATE TABLE `statistic_data`
 (
-    id         int          not null,
-    pubDate    bigint       null,
-    pubDateStr varchar(255) null,
-    title      varchar(255) null,
-    summary    varchar(255) null,
-    infoSource varchar(255) null,
-    sourceUrl  varchar(255) null,
-    provinceId int          null,
-    primary key (`id`) using btree
-) engine = InnoDB
-  default charset = utf8;
+    `id`                    int(11) NOT NULL AUTO_INCREMENT,
+    `confirmedCount`        int(11) NULL DEFAULT NULL,
+    `confirmedIncr`         int(11) NULL DEFAULT NULL,
+    `curedCount`            int(11) NULL DEFAULT NULL,
+    `curedIncr`             int(11) NULL DEFAULT NULL,
+    `currentConfirmedCount` int(11) NULL DEFAULT NULL,
+    `currentConfirmedIncr`  int(11) NULL DEFAULT NULL,
+    `dateId`                int(11) NULL DEFAULT NULL,
+    `deadCount`             int(11) NULL DEFAULT NULL,
+    `deadIncr`              int(11) NULL DEFAULT NULL,
+    `provinceId`            int(11) NULL DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE,
+    INDEX `index_statistic_data` (`provinceId`, `dateId`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 9544
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
 
-
-drop table if exists `country_info`;
-create table `country_info`
+-- ----------------------------
+-- Table structure for timeline
+-- ----------------------------
+DROP TABLE IF EXISTS `timeline`;
+CREATE TABLE `timeline`
 (
-    `locationId`            int(11)      not null,
-    `provinceName`          varchar(255) null default null,
-    `provinceShortName`     varchar(255) null default null,
-    `currentConfirmedCount` int(11)      null default null,
-    `confirmedCount`        int(11)      null default null,
-    `suspectedCount`        int(11)      null default null,
-    `curedCount`            int(11)      null default null,
-    `deadCount`             int(11)      null default null,
-    `modifyTime`            bigint(20)   null default null,
-    `createTime`            bigint(20)   null default null,
-    `countryShortCode`      varchar(255) null default null,
-    `countryType`           int(2)       null default null,
-    `continents`            varchar(255) null default null,
-    primary key (`locationId`) using btree
-) engine = InnoDB
-  default charset = utf8;
+    `id`         int(11)                                                 NOT NULL,
+    `pubDate`    bigint(20)                                              NULL DEFAULT NULL,
+    `pubDateStr` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    `title`      varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    `summary`    varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    `infoSource` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    `sourceUrl`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    `provinceId` int(11)                                                 NULL DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE,
+    INDEX `index_timeline_pubDate` (`pubDate`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- View structure for timeline_limit120
+-- ----------------------------
+DROP VIEW IF EXISTS `timeline_limit120`;
+CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `timeline_limit120` AS
+select `timeline`.`id`         AS `id`,
+       `timeline`.`pubDate`    AS `pubDate`,
+       `timeline`.`pubDateStr` AS `pubDateStr`,
+       `timeline`.`title`      AS `title`,
+       `timeline`.`summary`    AS `summary`,
+       `timeline`.`infoSource` AS `infoSource`,
+       `timeline`.`sourceUrl`  AS `sourceUrl`,
+       `timeline`.`provinceId` AS `provinceId`
+from `timeline`
+order by `timeline`.`pubDate` desc
+limit 120;
 
-CREATE VIEW timeline_limit120 AS
-SELECT *
-FROM timeline
-ORDER BY pubDate DESC
-LIMIT 120;
-
-
-
-SELECT *
-FROM timeline_limit120;
+SET FOREIGN_KEY_CHECKS = 1;
